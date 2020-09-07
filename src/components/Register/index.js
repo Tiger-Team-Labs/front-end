074672552ffdd6091a90681223616738f26e3e41
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-07 15:35:03 -0500

    Install npm

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,12 +1,16 @@
 //import react
 import React from 'react';
-import { FormControl, FormControlLabel } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
 
 //create and export register component
 export const Register = () => {
+  const [] = useState();
+  return (
     <FormControl>
-        <InputLabel htmlFor="my-input">Test1</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">you can make a test</FormHelperText>
+      <InputLabel htmlFor="my-input">Test1</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">you can make a test</FormHelperText>
     </FormControl>
+  );
 }
+// funcional component 
