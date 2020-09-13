44a321446c6ffa7c077115ef3e056920ec737fa2
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-12 19:43:17 -0500

    create styles file for register component

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,24 +1,11 @@
 //import react
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+//import custom styles
+import { useStyles } from './styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import { Button } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-	modal: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	paper: {
-		backgroundColor: theme.palette.background.paper,
-		border: '2px solid #000',
-		boxShadow: theme.shadows[5],
-		padding: theme.spacing(2, 4, 3),
-	},
-}));
 
 //create and export register component
 export const Register = () => {
