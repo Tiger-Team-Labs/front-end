4a7645034b444e501b3a5e04071c46a3124d603d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-12 21:32:29 -0500

    change styled component for custom hook create style

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,14 +3,9 @@ import React from 'react';
 //import custom styles
 import { useStyles } from './styles';
 //import material ui components
-import {
-	Button,
-	Modal,
-	Backdrop,
-	Fade,
-	FormGroup,
-	TextField,
-} from '@material-ui/core';
+import { Button, Modal, Backdrop, Fade, Input } from '@material-ui/core';
+//import styled components
+import { Form } from './styles';
 
 //create and export register component
 export const Register = () => {
@@ -32,21 +27,37 @@ export const Register = () => {
 	return (
 		//react fragment to return whole component
 		<>
+			{/*Button to display the modal*/}
 			<Button type='button' onClick={handleOpen}>
 				react-transition-group
 			</Button>
+			{/*Modal component*/}
 			<Modal
 				className={classes.modal}
-				open={open}
+				open={true}
 				onClose={handleClose}
 				closeAfterTransition
 				BackdropComponent={Backdrop}
 				BackdropProps={{
 					timeout: 500,
 				}}>
-				<Fade in={open}>
+				{/*Fade animation for the modals*/}
+				<Fade in={true}>
 					<div className={classes.paper}>
-						<FormGroup></FormGroup>
+						{/*Form styled component*/}
+						<Form>
+							<Input
+								placeholder='Name'
+								variant='filled'
+								className={classes.input}
+							/>
+							<Input placeholder='Email' className={classes.input} />
+							<Input
+								placeholder='Password'
+								className={classes.input}
+								type='password'
+							/>
+						</Form>
 					</div>
 				</Fade>
 			</Modal>
--- src/components/Register/styles.js
+++ src/components/Register/styles.js
@@ -1,5 +1,17 @@
 //import make styles from styed component
 import { makeStyles } from '@material-ui/core/styles';
+//import styled component
+import styled from 'styled-components';
+
+//create and export Form component
+export const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	font-size: 2rem;
+`;
+
+//create and export input label
+export const Label = styled.label``;
 
 //custom styles from the modal component and export it
 export const useStyles = makeStyles((theme) => ({
@@ -13,5 +25,9 @@ export const useStyles = makeStyles((theme) => ({
 		border: '.2rem solid #000',
 		boxShadow: theme.shadows[5],
 		padding: '2rem',
+	},
+	input: {
+		fontSize: '2rem !important',
+		margin: '1rem 0',
 	},
 }));
