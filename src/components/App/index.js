28d4c42cba2b6ccd6181a928ab0782a5fb2d0ec8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-18 19:33:54 -0500

    create the body component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 //import register component
 import { Header } from '../Header';
+//import body component
+import { Body } from '../Body';
 
 //create and export App component
 export const App = () => {
 	return (
 		<>
 			<Header />
+			<Body />
 		</>
 	);
 };
---
+++ src/components/Body/index.js
@@ -0,0 +1,7 @@
+//import react
+import React from 'react';
+
+//create body component and export it
+export const Body = () => {
+	return <h1>I'm the body</h1>;
+};
