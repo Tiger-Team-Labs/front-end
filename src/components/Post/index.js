79a688c4dfb5948b691b2184d97ed95422a192cb
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-21 19:05:26 -0500

    find the spefic post, and access to the data from the specic post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -12,20 +12,36 @@ export const Post = () => {
 	console.dir(params);
 	//use context
 	const { posts } = useContext(Context);
-	//use state to save the post specific post
-	const [post, setPost] = useState();
+	//use stat to save the post
+	const [post, setPost] = useState({ title: '', content: '' });
+	//use state to save the title specific post
+	const [title, setTitle] = useState('');
+	//use state to save the content of specific post
+	const [content, setContent] = useState('');
 
 	//use effect to find the post
 	useEffect(() => {
-		setPost(posts.filter((post) => post._id === params.post));
-	}, [params.post, posts]);
+		//save the post
+		setPost(posts.find((post) => post._id === params.post));
+	}, [params.post, post, posts]);
+
+	//use effect to set the title and the content
+	useEffect(() => {
+		//save the title
+		setTitle(post.title);
+		//save the content
+		setContent(post.content);
+	}, [post]);
+
+	console.log(post);
 
 	return (
 		<>
 			{/*post card*/}
 			{/*title*/}
+			<h1>{title}</h1>
 			{/*contents*/}
-			<h4>I'm the post</h4>
+			<h4>{content}</h4>
 		</>
 	);
 };
