8e8c625061c1d076fbe8fe79786ad6e53546f437
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-24 20:50:59 -0500

    set up dash board

--- src/pages/AdminView/index.js
+++ src/pages/AdminView/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems } from '../../components/ListItems';
 //import styles
 import { useStyles } from './styles';
@@ -45,7 +40,6 @@ export default function Dashboard() {
 	const handleDrawerClose = () => {
 		setOpen(false);
 	};
-	const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
 	return (
 		<div className={classes.root}>
@@ -71,13 +65,8 @@ export default function Dashboard() {
 						color='inherit'
 						noWrap
 						className={classes.title}>
-						Dashboard
+						Dashboard Foro App
 					</Typography>
-					<IconButton color='inherit'>
-						<Badge badgeContent={4} color='secondary'>
-							<NotificationsIcon />
-						</Badge>
-					</IconButton>
 				</Toolbar>
 			</AppBar>
 			<Drawer
@@ -97,25 +86,7 @@ export default function Dashboard() {
 			</Drawer>
 			<main className={classes.content}>
 				<div className={classes.appBarSpacer} />
-				<Container maxWidth='lg' className={classes.container}>
-					<Grid container spacing={3}>
-						{/* Chart */}
-						<Grid item xs={12} md={8} lg={9}>
-							<Paper className={fixedHeightPaper}></Paper>
-						</Grid>
-						{/* Recent Deposits */}
-						<Grid item xs={12} md={4} lg={3}>
-							<Paper className={fixedHeightPaper}></Paper>
-						</Grid>
-						{/* Recent Orders */}
-						<Grid item xs={12}>
-							<Paper className={classes.paper}></Paper>
-						</Grid>
-					</Grid>
-					<Box pt={4}>
-						<Copyright />
-					</Box>
-				</Container>
+				<Container maxWidth='lg' className={classes.container}></Container>
 			</main>
 		</div>
 	);
