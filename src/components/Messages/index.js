82e4abb38d11a82613331f166ed6bc7bd9eb4119
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-01 18:43:56 -0500

    set up comment component

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,7 +1,41 @@
 //import react
 import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 
-//create and import message component
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+	},
+	details: {
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	content: {
+		flex: '1 0 auto',
+	},
+}));
+
 export const Message = () => {
-	return <h1>Hey there</h1>;
+	const classes = useStyles();
+	const theme = useTheme();
+
+	return (
+		<Card className={classes.root}>
+			<div className={classes.details}>
+				<CardContent className={classes.content}>
+					<Typography component='h5' variant='h5'>
+						Comment
+					</Typography>
+				</CardContent>
+			</div>
+		</Card>
+	);
 };
