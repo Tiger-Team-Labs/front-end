8d40e895a77a0dc8d124b4b8e1be891d08a0f804
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-01 19:03:26 -0500

    finish the set up of commit component

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,16 +1,18 @@
 //import react
-import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
+//import input
+import Input from '@material-ui/core/Input';
+//import icons
+import DeleteIcon from '@material-ui/icons/Delete';
+import CreateIcon from '@material-ui/icons/Create';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
 	root: {
 		display: 'flex',
 	},
@@ -21,21 +23,48 @@ const useStyles = makeStyles((theme) => ({
 	content: {
 		flex: '1 0 auto',
 	},
+	input: {
+		padding: '.5rem',
+		margin: '.5em',
+	},
 }));
 
 export const Message = () => {
+	//use local state
+	const [isOpen, setIsOpen] = useState(false);
+	const [message, setMessage] = useState('example');
+	//use styles
 	const classes = useStyles();
-	const theme = useTheme();
+
+	const EditMessage = () => <Input className={classes.input} value={message} />;
 
 	return (
 		<Card className={classes.root}>
 			<div className={classes.details}>
-				<CardContent className={classes.content}>
-					<Typography component='h5' variant='h5'>
-						Comment
-					</Typography>
-				</CardContent>
+				{isOpen ? (
+					<EditMessage />
+				) : (
+					<CardContent className={classes.content}>
+						<Typography component='h5' variant='h5'>
+							Comment
+						</Typography>
+					</CardContent>
+				)}
 			</div>
+			{isOpen ? (
+				<IconButton onClick={() => setIsOpen((value) => (value = false))}>
+					<HighlightOffIcon />
+				</IconButton>
+			) : (
+				<>
+					<IconButton onClick={() => setIsOpen((value) => (value = true))}>
+						<CreateIcon />
+					</IconButton>
+					<IconButton>
+						<DeleteIcon />
+					</IconButton>
+				</>
+			)}
 		</Card>
 	);
 };
